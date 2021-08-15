20a8ed01bfe36490563b65d0e94b528d5bd38c51
Jhon Wilker <jhonwilkersousa@gmail.com>
2021-08-15 00:09:39 -0300

    alterações request api

--- lib/app/quizapp.dart
+++ lib/app/quizapp.dart
@@ -33,39 +33,17 @@ class _QuizAppState extends State<QuizApp> {
   }
 
   bool get temPerguntaSelecionada {
+    getPerguntas();
     return _perguntaSelecionada < _perguntas.length;
   }
 
-  Future<List<Map<String, Object>>> getPerguntas() async {
-    var url =
-        Uri.https('http://localhost:5000', '/perguntass', {'q': '{http}'});
-
+  void getPerguntas() async {
+    var url = Uri.http('10.0.2.2:5000', '/perguntas', {'q': '{http}'});
     var response = await http.get(url);
-    if (response.statusCode == 200) {
-      var perguntas = convert.jsonDecode(response.body);
-      return perguntas;
-    } else {
-      print('Request failed with status: ${response.statusCode}.');
-      var perguntas = const [
-        {
-          'texto': 'Qual a sua cor favorita?',
-          'respostas': [
-            {'texto': 'Azul', 'pontuacao': 10},
-            {'texto': 'Verde', 'pontuacao': 10},
-            {'texto': 'Preto', 'pontuacao': 10},
-          ]
-        },
-        {
-          'texto': 'Qual o seu animal favorito?',
-          'respostas': [
-            {'texto': 'Cão', 'pontuacao': 10},
-            {'texto': 'Gato', 'pontuacao': 10},
-            {'texto': 'Passáro', 'pontuacao': 10}
-          ]
-        },
-      ];
-      return perguntas;
-    }
+
+    //if(response.statusCode == 200) {
+    _perguntas = convert.jsonDecode(response.body) as Map<String, dynamic>;
+    print(_perguntas);
   }
 
   @override
